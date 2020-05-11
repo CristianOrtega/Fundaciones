ac24202d09dc7484f5f2e97b6309fbeed01ab3a4
Cristian Ortega <c.ortegamu@gmail.com>
2020-05-11 10:05:15 -0400

    eliminacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,10 +18,6 @@ const routes: Routes = [
         loadChildren: () => import('./modules/enumeration/enumeration.module').then(m => m.EnumerationModule)
       },
       {
-        path: 'externalcode',
-        loadChildren: () => import('./modules/code-external/code-external.module').then(m => m.CodeExternalModule)
-      }
-      {
         path: 'generalparameter',
         loadChildren: () => import('./modules/general-parameter/general-parameter.module').then(m => m.GeneralParameterModule)
       },
